13c02ca71f106c330bdee9ae6e73b7ae1c05be97
Mike Jerome <mjerome@financialforce.com>
2018-06-20 21:10:42 +0100

    Rufo config

--- config.rb
+++ config.rb
@@ -9,9 +9,9 @@ end
 # https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
 
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
