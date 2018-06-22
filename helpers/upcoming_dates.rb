b8576f26ba043dd830c74eac5822efc56107c535
Mike Jerome <mjerome@financialforce.com>
2018-06-22 23:01:24 +0100

    List an event if it runs today

--- helpers/upcoming_dates.rb
+++ helpers/upcoming_dates.rb
@@ -1,9 +1,23 @@
 require_relative "../lib/active_support_extensions/date_and_time/calculations"
 
 module UpcomingDates
+  WEEK_DAYS = {
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6,
+  }
+
   def upcoming_dates(day_of_week, num_of_weeks)
     upcoming_dates = []
     date = Date.today
+    if WEEK_DAYS.fetch(day_of_week) == date.wday
+      upcoming_dates.push(date)
+      num_of_weeks = num_of_weeks - 1
+    end
     num_of_weeks.times do
       date = date.next_occurring(day_of_week)
       upcoming_dates.push(date)
