d7641940e577f94c7a684600964e7bfdf547411c
Mike Jerome <m@j4e.name>
2022-05-13 17:42:31 +0100

    Remove sites from ruby-based version

--- lambda-to-trigger-build.js
+++
@@ -1,6 +0,0 @@
-const AWS = require("aws-sdk");
-exports.handler = async (event) => {
-  const codebuild = new AWS.CodeBuild();
-  const params = { projectName: "HelloYogaBuildProject" };
-  const request = await codebuild.startBuild(params).promise();
-};
